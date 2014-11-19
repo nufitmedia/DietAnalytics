a3621e9ff36ca5a7351eb40cf72fe5d24f6693b2
awilner88 <adamwilner@Adams-MacBook-Pro.local>
2014-11-19 14:58:35 -0500

    social media test

--- www/js/app.js
+++ www/js/app.js
@@ -69,19 +69,3 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
 });
-
-ionicApp.controller("ExampleController", function($scope, $cordovaSocialSharing) {
- 
-    $scope.shareAnywhere = function() {
-        $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "http://blog.nraboy.com");
-    }
- 
-    $scope.shareViaTwitter = function(message, image, link) {
-        $cordovaSocialSharing.canShareVia("twitter", message, image, link).then(function(result) {
-            $cordovaSocialSharing.shareViaTwitter(message, image, link);
-        }, function(error) {
-            alert("Cannot share on Twitter");
-        });
-    }
- 
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -74,4 +74,20 @@ angular.module('starter.controllers', [])
 .controller('ArticleCtrl', function($scope, $stateParams, Articles) {
 	$scope.article = Articles.get($stateParams.id);
 	$scope.myHTML = $scope.article.body;
+})
+
+.controller("ExampleController", function($scope, $cordovaSocialSharing) {
+ 
+    $scope.shareAnywhere = function() {
+        $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "http://blog.nraboy.com");
+    }
+ 
+    $scope.shareViaTwitter = function(message, image, link) {
+        $cordovaSocialSharing.canShareVia("twitter", message, image, link).then(function(result) {
+            $cordovaSocialSharing.shareViaTwitter(message, image, link);
+        }, function(error) {
+            alert("Cannot share on Twitter");
+        });
+    }
+ 
 });
