676b348825f78ced5bdece5fae455506d73f3871
awilner88 <adamwilner@Adams-MacBook-Pro.local>
2014-11-21 13:59:21 -0500

    test

--- www/js/app.js
+++ www/js/app.js
@@ -69,4 +69,31 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/articles');
+})
+
+.directive('dynFbCommentBox', function () {
+    function createHTML(href, numposts, colorscheme, width) {
+        return '<div class="fb-comments" ' +
+                       'data-href="' + href + '" ' +
+                       'data-numposts="' + numposts + '" ' +
+                       'data-colorsheme="' + colorscheme + '" ' +
+                       'data-width="' + width + '">' +
+               '</div>';
+    }
+
+
+    return {
+        restrict: 'A',
+        scope: {},
+        link: function postLink(scope, elem, attrs) {
+            attrs.$observe('pageHref', function (newValue) {
+                var href        = newValue;
+                var numposts    = attrs.numposts    || 5;
+                var colorscheme = attrs.colorscheme || 'light';
+                var width = attrs.width || '100%';
+                elem.html(createHTML(href, numposts, colorscheme, width));
+                FB.XFBML.parse(elem[0]);
+            });
+        }
+    };
 });
